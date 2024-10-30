b164c2abb9552ac3e64433585a7be6755955bbb0
konentung <84655730+konentung@users.noreply.github.com>
2024-10-30 15:06:45 +0800

    feature-部屬功能設定(補)

--- OnlineProgrammingPlatform/settings.py
+++ OnlineProgrammingPlatform/settings.py
@@ -29,9 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') if ('SECRET_KEY') in os.environ else p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 AUTH_USER_MODEL = 'accounts.Student'
