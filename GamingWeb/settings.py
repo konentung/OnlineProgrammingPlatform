61d51c532f2d8e2e53efadcb9c40d4365a3b5c8c
konentung <84655730+konentung@users.noreply.github.com>
2025-03-31 15:07:57 +0800

    set debug false

--- GamingWeb/settings.py
+++ GamingWeb/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_path):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
