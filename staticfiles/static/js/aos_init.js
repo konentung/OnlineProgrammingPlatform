c2f0fec89c605a4c29895e54597b716ae0a3974a
konentung <84655730+konentung@users.noreply.github.com>
2025-04-13 23:41:13 +0800

    CODINGPJ-58 製作幕間動畫 1. 放入所有影片(只缺演算村(上)) 2. 重新collect static

--- staticfiles/static/js/aos_init.js
+++
@@ -1,6 +0,0 @@
-AOS.init({
-    duration: 800,
-    once: true,
-    offset: 100
-  });
-  
--- staticfiles/static/js/kaboomCtx.f7a235bb0d5c.js
+++
@@ -1,2 +0,0 @@
-console.log("Checking window.k:", window.k);
-export const k = window.k || console.error("Kaboom.js is not initialized!");
